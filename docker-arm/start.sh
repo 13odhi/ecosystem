a537a818d84a22d84501fbe527f233f13303dbc2
ecosystem.Ai <34399709+ecosystemai@users.noreply.github.com>
2025-04-09 13:15:54 +0200

    Update start.sh

--- docker-arm/start.sh
+++ docker-arm/start.sh
@@ -1,5 +1,7 @@
 docker network create ecosystem
 
+export SERVER_IP=http://`hostname -I | awk '{print $1}'`
+
 docker compose --env-file .env -f docker-compose_arm.yaml down
 docker compose --env-file .env -f docker-compose_arm.yaml pull
 docker compose --env-file .env -f docker-compose_arm.yaml up -d 
